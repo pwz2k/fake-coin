92f21bb8b3ae60481ac649f0d9205e0d0ee9779f
pwz2k <pwz2k@hotmail.com>
2024-01-10 15:50:53 -0500

    Update Reviews.js

--- src/Reviews.js
+++ src/Reviews.js
@@ -11,27 +11,21 @@ const Reviews = () => {
         <div className="review-card-container">
           <div className="review-card">
             <p className="comment">
-              i bought this for my husband, he opened the mail before work & got
-              it everywhere! He hand to change, was late for work & might be
-              getting fired, lol!
+              I went all in so I can change my future. My wife's boyfriend is gonna love this!
             </p>
             <p className="customer-name">-Grady Chambers</p>
           </div>
           <div className="review-card">
             <p className="comment">
-              i bought this for my husband, he opened the mail before work & got
-              it everywhere! He hand to change, was late for work & might be
-              getting fired, lol!
+              I bought this with the hope of getting a Corvette. Stupid people can believe in anything, so I can believe in Blondie!
             </p>
-            <p className="customer-name">-Grady Chambers</p>
+            <p className="customer-name">-Chris Peacock</p>
           </div>
           <div className="review-card">
             <p className="comment">
-              i bought this for my husband, he opened the mail before work & got
-              it everywhere! He hand to change, was late for work & might be
-              getting fired, lol!
+              Diversify if you have $10M in the bank and you want to ensure you earn more than inflation. But to get to that you need to take risks. You need to pull off YOLO and pray.
             </p>
-            <p className="customer-name">-Grady Chambers</p>
+            <p className="customer-name">-Mike Hawk</p>
           </div>
         </div>
       </div>
