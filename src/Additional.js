d38e48bc382a1f08648a9910fb1cc57731e922b7
pwz2k <pwz2k@hotmail.com>
2024-07-10 16:00:15 -0400

    Update Additional.js

--- src/Additional.js
+++ src/Additional.js
@@ -15,13 +15,13 @@ const Additional = () => {
       </div>
       <div className="additional-links">
         <div className="additional-link">
-          <a href="#">HOW TO BUY</a>
+          <a href="https://app.uniswap.org/swap?chain=base&outputCurrency=0x37fc5e14aC765A5CeadA829A9Fc0D7d83d5de273">HOW TO BUY</a>
         </div>
         <div className="additional-social">
-          <a href="#" className="twitter">
+          <a href="https://x.com/ogblondieguy" className="twitter">
             <img src={AdditionalTwitter} alt="" />
           </a>
-          <a href="#" className="telegram">
+          <a href="https://t.me/blondiecoinofficial" className="telegram">
             <img src={AdditionalTelegram} alt="" />
           </a>
         </div>
