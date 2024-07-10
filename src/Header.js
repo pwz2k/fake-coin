0fc2ad545efa4c7ca991a05f9bc8383493e406fb
pwz2k <pwz2k@hotmail.com>
2024-07-10 15:32:49 -0400

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
             {/* Desktop Menu */}
             <Nav.Link href="#">ABOUT</Nav.Link>
             <Nav.Link href="#">ROADMAP</Nav.Link>
-            <Nav.Link href="#">HOW TO BUY</Nav.Link>
+            <Nav.Link href="https://app.uniswap.org/swap?chain=base&outputCurrency=0x37fc5e14aC765A5CeadA829A9Fc0D7d83d5de273">HOW TO BUY</Nav.Link>
           </div>
 
           <div className="social-icons">
